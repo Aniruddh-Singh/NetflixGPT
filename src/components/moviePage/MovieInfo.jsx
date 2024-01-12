082356e1c938b752c33e26b0277a860bcb20d0b1
Aniruddh-Singh <aniruddhsinghindia@gmail.com>
2024-01-12 13:12:55 +0530

    updated imports in movieInfo

--- src/components/moviePage/MovieInfo.jsx
+++ src/components/moviePage/MovieInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import MoviePoster from './moviePoster'
-import MovieDetails from './movieDetails'
+import MoviePoster from './MoviePoster'
+import MovieDetails from './MovieDetails'
 
 const MovieInfo = () => {
     return (
